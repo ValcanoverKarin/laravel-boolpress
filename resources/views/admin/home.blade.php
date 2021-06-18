94dd5fe1c44a484cff3d9a8db3d856106cc23b77
karin valcanover <valcanoverkarin@outlook.it>
2021-06-18 17:23:47 +0200

    modifiche

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,6 +5,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
+
+                <h1>Ciao sono la home solo per gli utenti loggati</h1>
+
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
