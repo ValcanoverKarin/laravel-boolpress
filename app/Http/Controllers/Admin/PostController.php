4d1a24bb8de32382bfaac94607f56463753b1281
karin valcanover <valcanoverkarin@outlook.it>
2021-06-21 16:49:37 +0200

    fatto create e store

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Post;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -31,7 +32,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.posts.create');
     }
 
     /**
@@ -42,7 +43,36 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required|max:255',
+            'content' => 'required|max:65000'
+        ]);
+
+        $new_post_data = $request->all();
+
+        //creao slug 
+        $new_slug = Str::slug($new_post_data['title'], '-');
+        $base_slug = $new_slug;
+        //controlla che non esista un post con questo slug
+        $post_with_existing_slug = Post::where('slug', '=', $new_slug)->first();
+        $counter = 1;
+
+        //se esiste tenta con altri slug
+        while($post_with_existing_slug) {
+            //Provo con un nuovo slug appendendo il counter
+            $new_slug = $base_slug . '-' . $counter;
+            $counter++;
+            //se anche il nuovo slug esiste nel db il while continua
+            $post_with_existing_slug = Post::where('slug', '=', $new_slug)->first();
+        }
+        //quando trova uno slug libero, popola il data da salvare
+        $new_post_data['slug'] = $new_slug;
+
+        $post = new Post();
+        $post->fill($new_post_data);
+        $post->save();
+
+        return redirect()->route('admin.posts.show', ['post' => $post->id]);
     }
 
     /**
